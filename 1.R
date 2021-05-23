648e4efd114ab613dd0d90bbd68531751ad48857
Juan Arredondo <juan.arredondo.c@usach.cl>
2021-05-22 20:52:44 -0400

    Agrego nuevamente

--- 1.R
+++ 1.R
@@ -12,16 +12,19 @@ library(car)
 library(lmtest)
 library(vcd)
 
-#1. Class: no-recurrence-events, recurrence-events
-#2. age: 10-19, 20-29, 30-39, 40-49, 50-59, 60-69, 70-79, 80-89, 90-99.
-#3. menopause: lt40, ge40, premeno.
-#4. tumor-size: 0-4, 5-9, 10-14, 15-19, 20-24, 25-29, 30-34, 35-39, 40-44, 45-49, 50-54, 55-59.
-#5. inv-nodes: 0-2, 3-5, 6-8, 9-11, 12-14, 15-17, 18-20, 21-23, 24-26, 27-29, 30-32, 33-35, 36-39.
-#6. node-caps: yes, no.
-#7. deg-malig: 1, 2, 3.
-#8. breast: left, right.
-#9. breast-quad: left-up, left-low, right-up, right-low, central.
-#10. irradiat: yes, no.
+#Los nombres originales de las variables, una breve explicación y los tipos de los datos:
+
+#age (rango de edad): 10-19, 20-29, 30-39, 40-49,.
+#menopause (momento de la menopausia): lt40, ge40, premeno.
+#tumor-size (tamaño del tumor extirpado en mm): 0-4, 5-9, 10-14, .
+#inv-nodes (una métrica de presencia de células cancerosas en los nodos linfáticos): 0-2, 3-5, 6-8, 9-11,.
+#node-caps (evidencia de que células cancerosas atravesaron la cápsula de los nódulos linfáticos): yes, no
+#deg-malig (grado histológico del tumor: bajo, intermedio, alto): 1, 2, 3.
+#breast (mama afectada): left, right.
+#breast-quad (cuadrante de la mama): left-up, left-low, right-up, right-low, central.
+#irradiat (radioterapia): yes, no.
+#Class (clase) Indica recurrencia, es la variable a predecir (no-recurrencia: 201 casos, recurrencia: 85 casos)
+
 
 # Leemos los datos
 dirstudio <- dirname(rstudioapi::getSourceEditorContext()$path)
@@ -52,7 +55,9 @@ tabla$breast <- as.factor(tabla$breast)
 tabla$breast.quad <- as.factor(tabla$breast.quad) 
 tabla$irradiat <- as.factor(tabla$irradiat)
 
-
+#Lo primero es ver el rango de las variables, algunas medidas
+#de tendencia central para las variables continuas, recuentos 
+#para las categóricas y presencia de valores faltantes.
 summary(tabla)
 
 #Se sacan los datos nulos del datagrama
@@ -64,6 +69,10 @@ tabla <- tabla[!bool.values,]
 
 summary(tabla)
 
+#Hay algunas variables que de antemano podemos sospechar 
+#que están relacionadas con otras. 
+#Un ejemplo de esto es la relación entre recurrencia 
+#con las demás variables
 attach(tabla)
 
 class_age <- table(class, age)
@@ -119,14 +128,14 @@ tablaDatos = matrix(c(cor.class_age$cramer, cor.class_menopause$creamer,
 
 #Se le asigna nombre a las columnas
 colnames(tablaDatos) =  c("Age",
-                     "Menopause",
-                     "Tumor.size",
-                     "Inv.nodes",
-                     "Node.caps",
-                     "Deg.malig",
-                     "Breast",
-                     "Breast.quad",
-                     "Irradiat")
+                          "Menopause",
+                          "Tumor.size",
+                          "Inv.nodes",
+                          "Node.caps",
+                          "Deg.malig",
+                          "Breast",
+                          "Breast.quad",
+                          "Irradiat")
 #y a las filas
 rownames(tablaDatos) = c("Class"
 )
@@ -135,9 +144,72 @@ rownames(tablaDatos) = c("Class"
 #Finalmente se convierte a tabla y se muestra
 tablaDeCorrelaciones=as.table(tablaDatos)
 tablaDeCorrelaciones
-barplot(tablaDeCorrelaciones, names.arg = colnames(tablaDatos))
 
-#Se realiza gráfico de cajas para las tablas.
+tablaDeCorrelaciones <- as.data.frame(tablaDeCorrelaciones)
+
+library (ggplot2) 
+
+#Gráfico de las correlaciones de los distintos atributos con respecto a la clase
+
+ggplot(data=tablaDeCorrelaciones, aes(x=reorder(Var2, -Freq), y=Freq, fill = Var2)) + 
+  geom_bar(stat="identity", position="stack")+ theme_minimal()
+
+
+class_ageGrafico <- as.data.frame(class_age)
+
+#Gráfico de frecuencias de edades dependiendo el tipo de clase
+ggplot(data=class_ageGrafico, aes(x=age , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_menopauseGrafico <- as.data.frame(class_menopause)
+
+#Gráfico de frecuencias de tipo de menopausia dependiendo el tipo de clase
+ggplot(data=class_menopauseGrafico, aes(x=menopause , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_tumor.sizeGrafico <- as.data.frame(class_tumor.size)
+
+#Gráfico de frecuencias de tamaño de tumor dependiendo el tipo de clase
+ggplot(data=class_tumor.sizeGrafico, aes(x=tumor.size , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_inv.nodesGrafico <- as.data.frame(class_inv.nodes)
+
+#Gráfico de frecuencias de inv.nodes dependiendo el tipo de clase
+ggplot(data=class_inv.nodesGrafico, aes(x=inv.nodes , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_node.capsGrafico <- as.data.frame(class_node.caps)
+
+#Gráfico de frecuencias de node.caps dependiendo el tipo de clase
+ggplot(data=class_node.capsGrafico, aes(x=node.caps , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_deg.maligGrafico <- as.data.frame(class_deg.malig)
+
+#Gráfico de frecuencias de deg.malig dependiendo el tipo de clase
+ggplot(data=class_deg.maligGrafico, aes(x=deg.malig , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_breastGrafico <- as.data.frame(class_breast)
+
+#Gráfico de frecuencias de breast dependiendo el tipo de clase
+ggplot(data=class_breastGrafico, aes(x=breast , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_breast.quadGrafico <- as.data.frame(class_breast.quad)
+
+#Gráfico de frecuencias de breast.quad dependiendo el tipo de clase
+ggplot(data=class_breast.quadGrafico, aes(x=breast.quad , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+class_irradiatGrafico <- as.data.frame(class_irradiat)
+
+#Gráfico de frecuencias de breast dependiendo el tipo de clase
+ggplot(data=class_irradiatGrafico, aes(x=irradiat , y=Freq, fill = class)) + 
+  geom_bar(stat="identity", position=position_dodge())+ theme_minimal()+ theme_classic()
+
+#Se realiza anova para las tablas.
 dl.class_age <- data.frame(class_age)
 p1 <- ggboxplot(
   dl.class_age,
@@ -145,9 +217,17 @@ p1 <- ggboxplot(
   xlab = "Rango de años", ylab = "Cantidad de individuos",
   color = "class",
   add = "jitter",
-  add.params = list(color = "class", fill = "class")
+  add.params = list(color = "class", fill = "class"),
 )
 print(p1)
+aov.class_age <- ezANOVA(
+  data = dl.class_age, 
+  dv = Freq,
+  wid = age,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_menopause <- data.frame(class_menopause)
 p2 <- ggboxplot(
@@ -159,6 +239,14 @@ p2 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p2)
+aov.class_menopause <- ezANOVA(
+  data = dl.class_menopause, 
+  dv = Freq,
+  wid = menopause,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_tumor.size <- data.frame(class_tumor.size)
 p3 <- ggboxplot(
@@ -170,6 +258,14 @@ p3 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p3)
+aov.class_tumor.size <- ezANOVA(
+  data = dl.class_tumor.size, 
+  dv = Freq,
+  wid = tumor.size,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_inv.nodes <- data.frame(class_inv.nodes)
 p4 <- ggboxplot(
@@ -181,6 +277,14 @@ p4 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p4)
+aov.class_inv.nodes <- ezANOVA(
+  data = dl.class_inv.nodes, 
+  dv = Freq,
+  wid = inv.nodes,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_node.caps <- data.frame(class_node.caps)
 p5 <- ggboxplot(
@@ -192,6 +296,14 @@ p5 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p5)
+aov.class_node.caps <- ezANOVA(
+  data = dl.class_node.caps, 
+  dv = Freq,
+  wid = node.caps,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_deg.malig <- data.frame(class_deg.malig)
 p6 <- ggboxplot(
@@ -203,6 +315,14 @@ p6 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p6)
+aov.class_deg.malig <- ezANOVA(
+  data = dl.class_deg.malig, 
+  dv = Freq,
+  wid = deg.malig,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_breast <- data.frame(class_breast)
 p7 <- ggboxplot(
@@ -214,6 +334,14 @@ p7 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p7)
+aov.class_breast <- ezANOVA(
+  data = dl.class_breast, 
+  dv = Freq,
+  wid = breast,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_breast.quad <- data.frame(class_breast.quad)
 p8 <- ggboxplot(
@@ -225,6 +353,14 @@ p8 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p8)
+aov.class_breast.quad <- ezANOVA(
+  data = dl.class_breast.quad, 
+  dv = Freq,
+  wid = breast.quad,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
 
 dl.class_irradiat <- data.frame(class_irradiat)
 p9 <- ggboxplot(
@@ -236,3 +372,62 @@ p9 <- ggboxplot(
   add.params = list(color = "class", fill = "class")
 )
 print(p9)
+aov.class_irradiat <- ezANOVA(
+  data = dl.class_irradiat, 
+  dv = Freq,
+  wid = irradiat,
+  within = class,
+  type = 3,
+  return_aov = TRUE
+)
+
+#Se puede análizar este test para calcular la asociación
+with(as.data.frame(tabla), fisher.test( table(class, irradiat)))
+
+#Esto indica que hay una asociación significativa,
+#pero antes de declarar esto, tenemos que considerar 
+#que hay otras variables que pueden ser importante, y que 
+#además pueden presentar co-linealidad. Por lo tanto, 
+#para estar seguros de esta asociación, tenemos que controlar 
+#las otras variables. Esto se puede probar con regresión 
+#logística, que además sirve como método de clasificación.
+
+#Cálculamos las distintas asociaciones entre variables
+#con respecto a la clase
+
+with(as.data.frame(tabla), fisher.test( table(class, age)))
+
+with(as.data.frame(tabla), fisher.test( table(class, menopause)))
+
+with(as.data.frame(tabla), fisher.test( table(class, inv.nodes)))
+
+with(as.data.frame(tabla), fisher.test( table(class, node.caps)))
+
+with(as.data.frame(tabla), fisher.test( table(class, deg.malig)))
+
+with(as.data.frame(tabla), fisher.test( table(class, breast)))
+
+with(as.data.frame(tabla), fisher.test( table(class, breast.quad)))
+
+#Resumen
+#Hasta el momento sabemos que hay variables
+#que no parecen estar aociadas con la probabilidad de recurrencia:
+
+#Edad
+#Menopausia
+#Mama
+#Cuadrante
+#Y otras variables que sí parecen ser importantes:
+
+#Tamaño del tumor
+
+#Cantidad de nódulos comprometidos 
+#Grado de malignidad (histológico)
+#Cápsula (celulas cancerosas fuera de las cápsulas de los nódulos)
+#Radioterapia
+
+#Algunas de las variables no importantes, 
+#pueden serlo cuando se las considera en conjunto con otras
+#(interacciones), y algunas variables pueden presentar 
+#colinealidad entre ellas; es decir, que están asociadas 
+#entre ellas.
